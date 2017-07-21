142c71a9f2c241a3ebb92523344539e6e06dc96d
egirlasm <egirlasm@gmail.com>
2017-07-21 22:13:07 +0900

    twice commit

---
+++ appendPayload/AppendPayLoad.hpp
@@ -0,0 +1,10 @@
+#ifndef __APPENDPAYLOAD_APPENDPAYLOAD__H__
+#define __APPENDPAYLOAD_APPENDPAYLOAD__H__
+
+#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
+#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
+#endif						
+
+#include <tchar.h>
+
+#endif // __APPENDPAYLOAD_APPENDPAYLOAD__H__
